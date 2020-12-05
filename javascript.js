9e7ffb2b6ecd33e5536f8d0b36b2540ec81ba1d7
Chris Rico <chris.r.rico@gmail.com>
2020-12-05 12:37:03 -0600

    cleaning up the get question function. make more readable code

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@
 // var totalQuestions = quizQuestions.length;
 // var timeEl = document.querySelector(".time");
 
-var currentQuestionIndex = 0;
+var questionNumber = 0;
 var time = quizQuestions.length * 15;
 var timerId;
 
@@ -41,46 +41,48 @@ function startQuiz(){
 //   // var imgEl = document.createElement("img");
 // }
 var results = [];
-function getQuestions(questionNumber) {
+function getQuestions() {
 
-  var qEl = document.getElementById("questionTitle") // = <div></div>
   var currentQuestion = quizQuestions[questionNumber];
+
+  var titleEl = document.getElementById("questionTitle") // = <div></div>
+  titleEl.textContent = currentQuestion.question
   // console.log("get questions",currentQuestion);
   var choices = document.getElementById("choices") 
   choices.classList.remove("hide");
 
-  for( var i = 0; i < currentQuestion.choices.length; i++) {
+  // for( var i = 0; i < currentQuestion.choices.length; i++) {
     // console.log('test', currentQuestion.choices[i])
-    var para = document.createElement("p");
-    para.className = 'choice';
+    // var para = document.createElement("p");
+    // para.className = 'choice';
     // onlick handler for the p element, once clicked it would compare the value of the text of the P elmeent currentQuestion.answer
-    para.onclick = function(events){
-      var isCorrect = events.target.innerText === currentQuestion.answer
-      var isLast = quizQuestions.length - 1 === questionNumber;
-      choices.innerHTML = '';
-      results.push(isCorrect);
+    // para.onclick = function(events){
+    //   var isCorrect = events.target.innerText === currentQuestion.answer
+    //   var isLast = quizQuestions.length - 1 === questionNumber;
+    //   choices.innerHTML = '';
+    //   results.push(isCorrect);
       // if this isn't the last question, call getQuestion on next question number
-      getQuestions(questionNumber + 1)
+      // getQuestions(questionNumber + 1)
 
        // otherwise print out score and tell the user to send endQuiz()
-    }
-    var node = document.createTextNode(currentQuestion.choices[i]);
-    para.appendChild(node);
-    choices.appendChild(para);
+    // }
+    // var node = document.createTextNode(currentQuestion.choices[i]);
+    // para.appendChild(node);
+    // choices.appendChild(para);
   }
   // Create a function to match answers against chosen answer by user.
 
   // if it is correct add to score then iterate currentQuestionIndex, else subtract score then iterate currentQuestionIndex
   // call this function in the getQuestions function
     //console.log(currentQuestion.question);
-    var questions = currentQuestion.question
-    qEl.textContent = questions;// <div> our questions text goes in here </div>
+    // var questions = currentQuestion.question
+    // qEl.textContent = questions;// <div> our questions text goes in here </div>
   
     
 
 //console.log(currentQuestion.choices);
 //console.log(currentQuestion);
-}
+// }
 function endQuiz() {
 
   clearInterval(timerId);
